a88fbcc37b061f60c05f9820c2cac34f62093ffb
AzureAD\SORATHORNKHUNPETCHNA <sorathorn.kh@mail.kmutt.ac.th>
2021-10-03 02:02:57 +0700

    exam

---
+++ ex1.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+enum Menu
+{
+    Play = 1,
+    Exist
+}
+
+namespace Hangman
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Header();
+            //ใส่คำศัพท์ให้ทาย
+            string[] listwords = new string[3];
+            listwords[0] = "tennis";
+            listwords[1] = "football";
+            listwords[2] = "badminton";
+            Random number = new Random();
+            var dimention = number.Next(0, 3);
+            string secretWord = listwords[dimention];
+            char[] guess = new char[secretWord.Length];
+            PlayGame();
+
+            for (int p = 0; p < secretWord.Length; p++)
+                guess[p] = '_';
+
+            while (true)
+            {
+                char playerGuess = char.Parse(Console.ReadLine());
+                for (int j = 0; j < secretWord.Length; j++)
+                {
+                    if (playerGuess == secretWord[j])
+                        guess[j] = playerGuess;
+                }
+                Console.WriteLine(guess);
+            }
+            static void Header()
+            {
+                //หน้าเริ่มต้น
+                Console.WriteLine("Welcome to Hangman Game");
+                Console.WriteLine("----------------------------------");
+                Console.WriteLine("Play Game");
+                Console.WriteLine("Exist");
+                Console.Write("Please Select Menu: ");
+            }
+            static void PlayGame()
+            {
+                //หน้าเกม
+                Console.WriteLine("Play Game Hangman");
+                Console.WriteLine("----------------------------");
+                Console.WriteLine("Incorrect Score: 0");
+                Console.WriteLine("----------------------------");
+                Console.WriteLine("Input letter alphabet");
+            }
+
+        }
+    }
+}
---
+++ ex2.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+
+enum Menu
+{
+    Login = 1,
+    Register
+}
+    
+
+namespace _2EX
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+
+        static void PrintMenuScreen()
+        {
+            //หัวข้อ
+            Console.WriteLine("Welcome to Digital Library");
+            Console.WriteLine("------------------------------------------");           
+        }
+
+        static void PrintListMenu()
+        {
+            //เลือกหน้า
+            Console.WriteLine("1.Login");
+            Console.WriteLine("2.Register");           
+        }
+
+        static void InputMenu()
+        {
+            //ใส่เลขเมนู
+            Console.WriteLine("Select menu: ");
+            Menu menu = (Menu)(int.Parse(Console.ReadLine()));
+            PresentMenu(menu);
+        }
+        static void PresentMenu(Menu menu)
+        {
+            //เปลี่ยนหน้า
+            if (menu = Menu.Login)
+            {
+                ShowLogin();
+            }
+            if else (menu = Menu.Register)
+            {
+                ShowRegister();
+            }
+            else
+            {
+                ShowEror();
+            }
+        }
+        static void ShowLogin()
+        {
+            
+        }
+        static void ShowRegister()
+        {
+
+        }
+        static void ShowEror()
+        {
+            //ให้ใส่ใหม่อีกรอบ
+            Console.Clear();
+            Console.WriteLine("Menu Incorrect Please try again.");
+            InputMenu();
+        }
+    }
+
+    class Person
+    {
+        protected string Name;
+        protected string Password;       
+
+        public Person(string Name, string Password)
+        {
+            this.Name = "name";
+            this.Password = "Password";           
+        }
+    }
+
+    class Student:Person
+    {
+        private string StudentId;
+        //คุณสมบัติของนักเรียน
+        public Student(string Name, string Password, string StudentId)
+        : base(Name, Password)
+        {
+            this.StudentId = StudentId;
+        }
+    }
+
+    class Employee:Person
+    {
+        private string EmployeeId;
+        //คุณสมบัติของEmployee
+        public Employee(string Name, string Password, string EmployeeId)
+        : base(Name, Password)
+        {
+            this.EmployeeId = EmployeeId;
+        }
+    }
+
+    class PersonList
+    {
+        private List<Person> personList;
+
+        public PersonList()
+        {
+            this.personList = new List<Person>();
+        }
+
+        public void AddNewPerson(Person person)
+        {
+            this.personList.Add(person);
+        }
+    }
+
+    class BookList
+    {
+
+    }
+}
+
